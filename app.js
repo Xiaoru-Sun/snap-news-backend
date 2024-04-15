88433301ba840f98ad8de83281114f7b5e0f2840
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-04-15 13:28:08 +0100

    adds route handler for all invalid routes

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-const { getTopics } = require("./controll/controller")
+const { getTopics} = require("./controll/controller")
 
 
 app.use(express.json());
 
 app.get('/api/topics', getTopics)
+
+//route for handling all invalid route
+app.get("*", (req, res, next) => {
+    res.status(404).send({msg : "Not found"})
+})
+
+app.use((err, req, res, next) => {
+    if(err.statusCode && err.msg){
+        res.status(err.statusCode).send({msg: err.msg})
+    } else {
+        next(err);
+    }
+})
+
+
 
 module.exports = app;
