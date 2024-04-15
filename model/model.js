d6dc7a7d61871c9add2cfbbd1cdcc7d259501a51
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-04-15 16:12:51 +0100

    adds fetchArticleById function to interact with db accroding to the validity of article_id

--- model/model.js
+++ model/model.js
@@ -6,5 +6,17 @@ function fetchTopics(){
     })
 }
 
+function fetchArticleById(article_id){
+    const sqlStr = `SELECT * FROM articles WHERE article_id=$1;`
+    return db.query(sqlStr, [article_id]).then(({rows}) => {
+        if (rows.length === 0){
+            return Promise.reject({
+                status : 404,
+                msg: "Not found, article_id does not exist"
+            })
+        }
+        return rows[0];
+    })
+}
 
-module.exports = { fetchTopics }
+module.exports = { fetchTopics, fetchArticleById }
