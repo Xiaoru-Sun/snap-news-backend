fc9a06324c4b2e364c0963f37589cc0e008f6f39
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-04-15 15:04:45 +0100

    adds getApi function to read engpoints.json file and send response accrodingly

--- controll/controller.js
+++ controll/controller.js
@@ -1,4 +1,5 @@
 const { fetchTopics } = require("../model/model")
+const fs = require("fs/promises")
 
 
 function getTopics(req, res, next){
@@ -8,6 +9,12 @@ function getTopics(req, res, next){
 }
 
 
+function getApi(req, res, next){
+    return fs.readFile("./endpoints.json", "utf-8").then((fileContents) => {
+        //console.log(fileContents)
+        const parsedContents = JSON.parse(fileContents);
+        res.status(200).send({endpoints : parsedContents})
+    }).catch(next)
+}
 
-
-module.exports = { getTopics }
+module.exports = { getTopics, getApi }
