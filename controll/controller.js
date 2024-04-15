f5138634cb77891418bdd99f9b26c2f0f4eb0d2a
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-04-15 14:07:51 +0100

    removes require app, and puts allTopics on topics key in response body

--- controll/controller.js
+++ controll/controller.js
@@ -1,10 +1,9 @@
-const { app } = require("../app")
 const { fetchTopics } = require("../model/model")
 
 
 function getTopics(req, res, next){
     fetchTopics().then((allTopics) => {
-        res.status(200).send(allTopics)
+        res.status(200).send({topics : allTopics})
     })
 }
 
