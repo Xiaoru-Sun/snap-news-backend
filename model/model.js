2692b13dcfb0e21a34fb0f937b4f0bc578b085b7
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-04-17 12:11:20 +0100

    adds solution and tests for DELETE /api/comments/:comment_id

--- model/model.js
+++ model/model.js
@@ -74,6 +74,18 @@ function updateArticleById(article_id, inc_votes){
 
 }
 
+function deleteCommentById(comment_id){
+    const sqlStr = 'DELETE FROM comments WHERE comment_id = $1 RETURNING *;'
+    return db.query(sqlStr, [comment_id]).then(({rows}) => {
+        if(rows.length === 0){
+            return Promise.reject({
+                status: 404,
+                msg: "Comment_id not found"
+            })
+        }
+    })
+}
 
 
-module.exports = { fetchTopics, fetchArticleById, fetchArticles, fetchCommentsByArticleId, doesArticleExist, insertCommentsByArticleId, updateArticleById }
+
+module.exports = { fetchTopics, fetchArticleById, fetchArticles, fetchCommentsByArticleId, doesArticleExist, insertCommentsByArticleId, updateArticleById, deleteCommentById }
