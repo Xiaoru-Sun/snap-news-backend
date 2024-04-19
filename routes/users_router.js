e83af30e7cc804cdce99443de24d9ac03990cedc
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-04-19 21:41:06 +0100

    refactors routes to make userRouter handle both requests to root path and with username parameters

--- routes/users_router.js
+++ routes/users_router.js
@@ -1,7 +1,9 @@
 const { getUsers } = require("../controll/controller");
+const users = require("../db/data/test-data/users");
 
 const usersRouter = require("express").Router();
 
-usersRouter.get('/', getUsers);
+usersRouter.get('/:username?', getUsers);
+
 
 module.exports = usersRouter;
