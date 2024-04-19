3fcf099cf7e03fa3fdbec5a12ab8781b94b95922
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-04-19 22:29:25 +0100

    adds patch /api/comments/:comment_id

--- routes/comments_router.js
+++ routes/comments_router.js
@@ -1,6 +1,8 @@
 const commentsRouter = require("express").Router();
-const { removeCommentById } = require("../controll/controller")
+const { removeCommentById, patchCommentById } = require("../controll/controller")
 
 commentsRouter.delete("/:comment_id", removeCommentById)
+
+commentsRouter.patch("/:comment_id", patchCommentById)
 
 module.exports = commentsRouter;
